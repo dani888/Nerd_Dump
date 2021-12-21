944e65d4f281e50d304bf8f18558bfb139580a7b
Daniel Werminghausen <daniel.werminghausen.com>
2021-12-21 17:04:23 -0500

    second commit template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Route, Switch } from "react-router-dom";
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
+import Home from './pages/Home/Home'
+
 import './App.css';
+import './pages/Home/home.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          {/* <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route exact path="/weather">
+            <Projects/>
+          </Route>
+          <Route exact path="/games">
+            <Projects/>
+          </Route> */}
+        </Switch>
+        <Footer />
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+
+
+function Home() {
+  return (
+    <div className="homer">
+     <h1>Home Page</h1>
+    </div>
+  )
+}
+  
+export default Home;
