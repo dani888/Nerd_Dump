16a4b24e0101425f45244cea7815873129073179
Daniel Werminghausen <daniel.werminghausen.com>
2021-12-22 23:36:08 -0500

    update2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 const Footer = () => {
-    return(
-        <footer className="page-footer grey darken-4">
-            {/* <div className="footer-copyright"> */}
-            <div className="container">
-                <div className="centerfot">
-                    © NerdDump 2022
-                    {/* <a className="grey-text text-lighten-4 right" href="#!">More Links</a> */}
-                </div>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="page-footer grey #000000 black">
+      {/* <div className="footer-copyright"> */}
+      <div className="container">
+        <div className="centerfot">
+          © NerdDump 2022 Daniel Werminghasuen, Ashley Swank
+          {/* <a className="grey-text text-lighten-4 right" href="#!">More Links</a> */}
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,48 +1,79 @@
 import { Link } from "react-router-dom";
 import "materialize-css/dist/css/materialize.min.css";
+import witcher from "../public/images/witcher.jpg";
 
 const Nav = () => {
-    return (
-      <>
+  return (
+    <>
       <nav>
-        <div className="nav-wrapper grey darken-4">
-        <a href="/" className="brand-logo center">NerdDump</a>
-          <a href="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        <div className="nav-wrapper grey #000000 black">
+          <a href="/" className="brand-logo center">
+            NerdDump
+          </a>
+          <a href="/" data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
           <ul className="left hide-on-med-and-down">
-            <li><Link to="/">
-            <div>Home</div>
-                </Link></li>
-                <li><Link to="/about">
-            <div>About</div>
-                </Link></li>
-                <li><Link to="/weather">
-            <div>Weather</div>
-                </Link></li>
-                <li><Link to="/games">
-            <div>Games</div>
-                </Link></li>
+            <li>
+              <Link to="/">
+                <div>Home</div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
+                <div>About</div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/weather">
+                <div>Weather</div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/games">
+                <div>Games</div>
+              </Link>
+            </li>
+            <li>
+              {/* <Link to="/games"> */}
+              <div>
+                <img
+                  className="witcher"
+                  alt="witcher"
+                  src={witcher}
+                  href="#"
+                ></img>
+              </div>
+              {/* </Link> */}
+            </li>
           </ul>
-      </div>
-    </nav>
+        </div>
+      </nav>
 
-    <ul className="sidenav sidenav-close" id="mobile-demo">
-      <li><Link to="/">
-      <div>Home</div>
-          </Link></li>
-          <li><Link to="/about">
-      <div>About</div>
-          </Link></li>
-          <li><Link to="/weather">
-      <div>Weather</div>
-          </Link></li>
-          <li><Link to="/games">
-      <div>Games</div>
-          </Link></li>
-    </ul>
-        </>
-      );
-    };
-
-    
+      <ul className="sidenav sidenav-close" id="mobile-demo">
+        <li>
+          <Link to="/">
+            <div>Home</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <div>About</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/weather">
+            <div>Weather</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/games">
+            <div>Games</div>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 export default Nav;
